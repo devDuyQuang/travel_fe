7e2cc899614b4d5f0731afe6808af61dafeeecb4
Vo Duy Quang <voquagduy28@gmail.com>
2026-05-30 13:59:37 +0700

    Add booking tour feature

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -14,64 +14,87 @@ const menu_data: MenuItem[] = [
     {
         id: 1,
         title: "Home",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            // { link: "/", title: "Home One" },
-            // { link: "/home-two", title: "Home Two" },
-            { link: "/home-three", title: "Home Three" },
-            // { link: "/home-four", title: "Home Four" },
-            // { link: "/home-five", title: "Home Five" },
-            // { link: "/home-six", title: "Home Six" },
-            // { link: "/home-seven", title: "Home Seven" },
-        ],
+        link: "/home-three",
+        has_dropdown: false,
+
+        // has_dropdown: true,
+        // sub_menus: [
+        //   { link: "/home-three", title: "Home Three" },
+        // ],
     },
+
     {
         id: 2,
-        title: "Features",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            { link: "/hotel-grid", title: "Hotel Grid" },
-            // { link: "/map-listing", title: "Hotel Listing" },
-            { link: "/tour-grid-1", title: "Tour Grid One" },
-            // { link: "/tour-grid-2", title: "Tour Grid Two" },
-            { link: "/tour-details", title: "Tour Details One" },
-            // { link: "/tour-details-2", title: "Tour Details Two" },
-        ],
+        title: "About",
+        link: "/about",
+        has_dropdown: false,
+
+        // title: "Features",
+        // link: "#",
+        // has_dropdown: true,
+        // sub_menus: [
+        //   { link: "/hotel-grid", title: "Hotel Grid" },
+        //   { link: "/tour-grid-1", title: "Tour Grid One" },
+        //   { link: "/tour-details", title: "Tour Details One" },
+        // ],
     },
+
     {
         id: 3,
-        title: "Pages",
+        title: "Service",
         link: "#",
         has_dropdown: true,
         sub_menus: [
-            { link: "/about", title: "About" },
-            { link: "/team", title: "Team" },
-            { link: "/team-details", title: "Team Details" },
-            { link: "/shop", title: "Shop" },
-            { link: "/shop-details", title: "Shop Details" },
-            { link: "/cart", title: "Cart" },
-            { link: "/wishlist", title: "Wishlist" },
-            { link: "/checkout", title: "Checkout" },
-            { link: "/pricing", title: "Pricing" },
-            { link: "/faq", title: "Faq" },
-            { link: "/login", title: "Log In" },
-            { link: "/register", title: "Register" },
-            { link: "/no-found", title: "Error" },
+            {
+                link: "/tour-grid-1",
+                title: "Tour Grid",
+            },
+            {
+                link: "/tour-details",
+                title: "Tour Detail One",
+            },
         ],
+
+        // title: "Pages",
+        // sub_menus: [
+        //   { link: "/about", title: "About" },
+        //   { link: "/team", title: "Team" },
+        //   { link: "/team-details", title: "Team Details" },
+        //   { link: "/shop", title: "Shop" },
+        //   { link: "/shop-details", title: "Shop Details" },
+        //   { link: "/cart", title: "Cart" },
+        //   { link: "/wishlist", title: "Wishlist" },
+        //   { link: "/checkout", title: "Checkout" },
+        //   { link: "/pricing", title: "Pricing" },
+        //   { link: "/faq", title: "Faq" },
+        //   { link: "/login", title: "Log In" },
+        //   { link: "/register", title: "Register" },
+        //   { link: "/no-found", title: "Error" },
+        // ],
     },
+
     {
         id: 4,
         title: "Blogs",
         link: "#",
         has_dropdown: true,
         sub_menus: [
-            { link: "/blog-grid", title: "Blog Grid" },
-            { link: "/blog-standard", title: "Blog Standard" },
-            { link: "/blog-details", title: "Blog Details" },
+            {
+                link: "/blog-grid",
+                title: "Blog Grid",
+            },
+            {
+                link: "/blog-details",
+                title: "Blog Detail",
+            },
+
+            // {
+            //   link: "/blog-standard",
+            //   title: "Blog Standard",
+            // },
         ],
     },
+
     {
         id: 5,
         has_dropdown: false,
